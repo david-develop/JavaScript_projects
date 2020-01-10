ae4484016f163db2228d2d39abd56a85112fc021
david-develop <924@holbertonschool.com>
2020-01-09 22:08:40 -0500

    [Add] conditions programs

---
+++ 2-conditions/next_second_time_calc.js
@@ -0,0 +1,26 @@
+/* Write a program that asks for a time under the form of three information
+(hours, minutes, seconds). The program calculates and shows the time one
+second after. Incorrect inputs must be taken into account.
+*/
+
+let hour = Number(prompt("Enter the hours:"));
+let minute = Number(prompt("Enter the minutes:"));
+let second = Number(prompt("Enter the seconds:"));
+
+if ((hour < 24 && hour >= 0) && (minute < 60 && minute >= 0) && (second < 60 && second >= 0)) {
+    second++;
+    if (second === 60) {
+        second = 0;
+        minute++;
+        if (minute === 60) {
+            minute = 0;
+            hour++;
+            if (hour === 24) {
+                hour = 0;
+            }
+        }
+    }
+    console.log("In a second, time will be " + hour +" hours, " + minute + " minutes and " + second + " seconds.");
+} else {
+    console.log("Incorrect hour!");
+}
---
+++ 2-conditions/number_months.js
@@ -0,0 +1,44 @@
+// Write a program that accepts a month number (between 11 and 1212), then shows the number of days of that month. Leap years are excluded. Incorrect inputs must be taken into account.
+
+const month = Number(prompt("Enter month number :"));
+
+switch (month) {
+    case 1:
+        console.log("Number of days: 31");
+        break;
+    case 2:
+        console.log("Number of days: 28");
+        break;
+    case 3:
+        console.log("Number of days: 31");
+        break;
+    case 4:
+        console.log("Number of days: 30");
+        break;
+    case 5:
+        console.log("Number of days: 29");
+        break;
+    case 6:
+        console.log("Number of days: 30");
+        break;
+    case 7:
+        console.log("Number of days: 31");
+        break;
+    case 8:
+        console.log("Number of days: 31");
+        break;
+    case 9:
+        console.log("Number of days: 30");
+        break;
+    case 10:
+        console.log("Number of days: 31");
+        break;
+    case 11:
+        console.log("Number of days: 30");
+        break;
+    case 12:
+        console.log("Number of days: 31");
+        break;
+    default:
+        console.log("Not a valid month");
+}
